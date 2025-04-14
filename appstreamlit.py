6dafeba4991944c73d6d825a3e6b4565efe65229
AndresPonceM <151466098+AndresPonceM@users.noreply.github.com>
2025-04-14 00:33:17 -0500

    Update appstreamlit.py

--- appstreamlit.py
+++ appstreamlit.py
@@ -188,8 +188,7 @@ st.sidebar.subheader("Or select None if you want to upload your own image")
 uploaded_file = st.file_uploader("Upload your own image:", type=["jpg", "jpeg", "png"])
 
 if selected_example != "None" and selected_example:
-    del uploaded_file
-    gc.collect()
+    uploaded_file = None
     try:
         aviso = "Las imagenes terminadas en A pertenecen a Degeneración Macular (AMD), terminadas en D a Retinopatía Diabética (DR), y terminadas en N son de pacientes sanos (Normal). Al final se muestra el diagnóstico del clasificador"
         st.write(f"**Nota:** {aviso}")
